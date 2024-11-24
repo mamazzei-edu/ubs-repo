8586743987abdd3b0f8a152cf7ff8ccbb642eecd
Isah2525 <isaahvitoria25@gmail.com>
2024-11-23 21:57:44 -0300

    Criação do formulario de pacientes

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule para o *ngIf
+import { FormsModule } from '@angular/forms'; // Importa FormsModule para [(ngModel)]
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule], // Inclui CommonModule e FormsModule no array imports
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent {
-
+  nomeCompleto = '';
+  nomeSocial = '';
+  nomeMae = '';
+  nomePai = '';
+  dataNascimento = '';
+  sexo = '';
+  nacionalidade = '';
+  municipioNascimento = '';
+  racaCor = '';
+  frequentaEscola = '';
+  contatoCelular = '';
+  contatoResidencial = '';
+  contatoEmail = '';
 }
