6b93a00a606131ec6777e52296f0ccaf43389f71
SailorRafa <rafacerchi2005@gmail.com>
2024-11-25 21:00:49 -0300

    Campos adicionados!

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent {
+  
+  nomeCompleto = '';
+  nomeSocial = '';
+  nomeMae = '';
+  nomePai = '';
 
+  
+  dataNascimento = '';
+  sexo = '';
+
+  // Localidade
+  nacionalidade = '';
+  municipioNascimento = '';
+
+  // Dados Complementares
+  racaCor = '';
+  frequentaEscola = '';
+  escolaridade = '';
+  situacaoFamiliar = '';
+  vinculoEstabelecimento = '';
+  deficiencia = '';
+
+  // Dados de Contato
+  contatoCelular = '';
+  contatoResidencial = '';
+  contatoComercial = '';
+  contatoEmail = '';
+
+  // Documentos
+  cpf = '';
+  identidade = '';
 }
