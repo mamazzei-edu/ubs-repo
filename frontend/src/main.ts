06a7d0e3e186ea7e248f6ba56c39afc669d9afe4
Isah2525 <isaahvitoria25@gmail.com>
2024-11-30 23:44:45 -0300

    Alterações no front para padronização das telas

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const bootstrap = () => bootstrapApplication(AppComponent, config);
+
+bootstrap().catch((err) => console.error(err));
