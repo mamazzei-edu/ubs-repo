7a0dbc997118258ac7b4ba7fde1f850c446579dd
SailorRafaS2 <rafacerchi2005@gmail.com>
2025-03-10 22:50:20 -0300

    Novo Design + Validação de Campo Obrigatório

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,10 +1,11 @@
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RouterModule } from '@angular/router'; // Para funcionalidades de roteamento
 import { PacienteService } from '../service/paciente.service';
-
+ 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
@@ -14,18 +15,18 @@ import { PacienteService } from '../service/paciente.service';
   providers: [PacienteService]
 })
 export class CadastroComponent {
-  
+ 
   nomeCompleto: string = '';
   nomeSocial: string = '';
   nomeMae: string = '';
   nomePai: string = '';
   dataNascimento: string = '';
   sexo: string = '';
-
+ 
   // Localidade
   nacionalidade: string = '';
   municipioNascimento: string = '';
-
+ 
   // Dados Complementares
   racaCor: string = '';
   frequentaEscola: string = '';
@@ -33,24 +34,30 @@ export class CadastroComponent {
   situacaoFamiliar: string = '';
   vinculoEstabelecimento: string = '';
   deficiencia: string = '';
-
+ 
   // Dados de Contato
   contatoCelular: string = '';
   contatoResidencial: string = '';
   contatoComercial: string = '';
   contatoEmail: string = '';
-
+ 
   // Documentos
   cpf: string = '';
-
+ 
   // Variáveis para controle de mensagem de sucesso ou erro
   showModal: boolean = false;
   modalMessage: string = '';
   isModalVisible: boolean = false;
-
+ 
   constructor(private http: HttpClient) {}
-
+ 
   salvarPaciente() {
+    // Verificar se o nomeCompleto está vazio ou contém apenas espaços em branco
+    if (!this.nomeCompleto || this.nomeCompleto.trim() === '') {
+      alert('Nome Completo é obrigatório!');
+      return; // Não continua com a execução
+    }
+ 
     const paciente = {
       nomeCompleto: this.nomeCompleto,
       nomeSocial: this.nomeSocial,
@@ -72,6 +79,8 @@ export class CadastroComponent {
       contatoEmail: this.contatoEmail,
       cpf: this.cpf,
     };
+ 
+    // Envia o paciente para a API
     this.http.post('http://localhost:8090/api/pacientes', paciente).subscribe({
       next: (data) => {
         console.log('Paciente salvo com sucesso:', data);
@@ -82,11 +91,11 @@ export class CadastroComponent {
       },
     });
   }
-
+ 
   openModal() {
     this.isModalVisible = true;
   }
-
+ 
   closeModal() {
     this.isModalVisible = false;
   }
