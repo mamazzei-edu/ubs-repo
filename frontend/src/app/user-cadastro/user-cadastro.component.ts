12836ed39cc6f60dc92ee323fdc32abe95b00f9b
Mathcerra123 <matheus.cerratotavares@gmail.com>
2025-06-30 18:51:51 -0300

    Botao criado para tela de lista de cadastro de usuario

--- frontend/src/app/user-cadastro/user-cadastro.component.ts
+++ frontend/src/app/user-cadastro/user-cadastro.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-cadastro',
   imports: [],
+  standalone: true,
   templateUrl: './user-cadastro.component.html',
   styleUrl: './user-cadastro.component.css'
 })
