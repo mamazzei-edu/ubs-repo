7f619869d76023d221ff758361c80149430dcad4
vitjmmf <victorjosemmfonseca@gmail.com>
2025-06-30 13:46:38 -0300

    cadastro funcionario

--- backend/src/main/java/br/sp/gov/fatec/ubs/backend/model/Cargo.java
+++
@@ -1,5 +0,0 @@
-package br.sp.gov.fatec.ubs.backend.model;
-
-public class Cargo {
-
-}
---
+++ backend/src/main/java/br/sp/gov/fatec/ubs/backend/model/UsuarioController.java
@@ -0,0 +1,82 @@
+package br.sp.gov.fatec.ubs.backend.model;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioSalvo = usuarioService.salvarUsuario(usuario);
+        return new ResponseEntity<>(usuarioSalvo, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
+        List<Usuario> usuarios = usuarioService.listarUsuarios();
+        return new ResponseEntity<>(usuarios, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarUsuario(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.buscarUsuarioPorId(id);
+        if (usuario.isPresent()) {
+            return new ResponseEntity<>(usuario.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Endpoint para excluir usuário
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirUsuario(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.buscarUsuarioPorId(id);
+        if (usuario.isPresent()) {
+            usuarioService.excluirUsuario(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Endpoint para atualizar um usuário
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarUsuario(
+            @PathVariable Long id, @RequestBody Usuario usuarioAtualizado) {
+        Optional<Usuario> usuarioOpt = usuarioService.buscarUsuarioPorId(id);
+
+        if (usuarioOpt.isPresent()) {
+            Usuario usuario = usuarioOpt.get();
+
+            // Atualizando os campos
+            usuario.setNomeCompleto(usuarioAtualizado.getNomeCompleto());
+            usuario.setMatricula(usuarioAtualizado.getMatricula());
+            usuario.setEmail(usuarioAtualizado.getEmail());
+            usuario.setNomeUsuario(usuarioAtualizado.getNomeUsuario());
+            usuario.setSenha(usuarioAtualizado.getSenha());
+            // Atualize outros campos conforme necessário...
+
+            Usuario usuarioAtualizadoSalvo = usuarioService.salvarUsuario(usuario);
+            return new ResponseEntity<>(usuarioAtualizadoSalvo, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
